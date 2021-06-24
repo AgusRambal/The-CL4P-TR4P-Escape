58770ac6bee4abf8c07dfa2971eb68e8cde80795
Agustin Rambaldo <agustin.rc97@gmail.com>
2021-06-24 00:06:42 -0300

    Seventh Commit
    Agregado menu principal, seleccion de nivel, y todo el traspaso de escenas funcional. Falta el arreglo del TimeRewind

---
+++ Assets/Scripts/MenuYLevelSelectScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuYLevelSelectScript : MonoBehaviour
+{
+    public Button level02Button, level03Button, playButton, levelSelectButton, backToMenu;
+    int levelPassed;
+    void Start()
+    {
+        levelPassed = PlayerPrefs.GetInt("LevelPassed");
+        level02Button.interactable = true;
+        level03Button.interactable = false;
+        playButton.interactable = true;
+        levelSelectButton.interactable = true;
+        backToMenu.interactable = true;
+
+        switch (levelPassed)
+        {
+            case 1:
+                level02Button.interactable = true;
+                break;
+            case 2:
+                level02Button.interactable = true;
+                level03Button.interactable = true;
+                break;
+        }
+    }
+
+    public void levelToLoad(int level)
+    {
+        SceneManager.LoadScene(level);
+    }
+
+    public void resetPlayerPrefs()
+    {
+        level02Button.interactable = true;
+        level03Button.interactable = false;
+        PlayerPrefs.DeleteAll();
+    }
+}

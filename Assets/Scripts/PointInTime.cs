ea641dbc4137b9517d71cd4eedd4daae477f46df
Agustin Rambaldo <agustin.rc97@gmail.com>
2021-06-23 22:21:46 -0300

    Sixth Commit
    Funciona bien el RewindTime, pero vuelve a la misma posicion donde presione, falta solucionar este problema

---
+++ Assets/Scripts/PointInTime.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class PointInTime
+{
+    public Vector3 Position;
+    public Vector3 Velocity;
+
+    public PointInTime(Vector3 Position, Vector3 Velocity)
+    {
+        this.Position = Position;
+        this.Velocity = Velocity;
+    }
+}
---
+++ Assets/Scripts/RewindTime.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RewindTime : MonoBehaviour
+{
+    public bool isRewinding = false;
+
+    List<PointInTime> pointsInTime = new List<PointInTime>();
+
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            StartRewind();
+        }
+        if (Input.GetKeyUp(KeyCode.K))
+        {
+            StopRewind();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (isRewinding)
+        {
+            Rewind();
+        }
+        else
+        {
+            Record();
+        }
+    }
+
+    void Rewind()
+    {
+        if (pointsInTime.Count>0)
+        {
+           SetPointInTime(pointsInTime[pointsInTime.Count - 1]);
+            pointsInTime.RemoveAt(pointsInTime.Count -1);
+        }
+        else
+        {
+            StopRewind();
+        }
+        
+    }
+
+    void Record()
+    {
+        PointInTime timePoint = new PointInTime(transform.position, rb.velocity);
+        pointsInTime.Add(timePoint);
+    }
+
+    public void StartRewind()
+    {
+        isRewinding = true;
+    }
+
+    public void StopRewind()
+    {
+        isRewinding = false;
+    }
+
+    void SetPointInTime(PointInTime timePoint)
+    {
+        this.transform.position = timePoint.Position;
+        rb.velocity = timePoint.Velocity;
+    }
+
+}
